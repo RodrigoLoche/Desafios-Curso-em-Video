9e0d57f07f447dddbcd6939f35a96bf7c60a8c60
Rodrigo Loche <rodrigoloche@gmail.com>
2025-12-23 14:55:36 +0900

    002

---
+++ desafio_02.py
@@ -0,0 +1,10 @@
+# Desafio 02
+
+# Crie um script Python que leia o dia, o mês e o ano 
+# de nascimento de uma pessoa e mostre uma mensagem com a data formatada.
+
+dia = input("Digite o DIA do seu aniversário: ")
+mes = input("Digite o MÊS do seu aniversário: ")
+ano = input("Digite o ANO do seu aniversário: ")
+
+print(f"Você nasceu em {dia}/{mes}/{ano}")
