97043220db2a2c1ff3536962329d25950b19b92e
Rodrigo Loche <rodrigoloche@gmail.com>
2025-12-23 14:58:09 +0900

    005

---
+++ desafio_05.py
@@ -0,0 +1,8 @@
+# Desafio 005
+#Faça um programa que leia um número inteiro e mostre na tela
+# o seu sucessor e seu antecessor.
+
+num = int(input("Digite um número: "))
+antecessor = num - 1
+sucessor = num + 1
+print(f"O antecessor de {num} é {antecessor} e o sucessor é {sucessor}")
