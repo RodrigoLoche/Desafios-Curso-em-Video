c953b22850e14e79b78370a23e8f88e6342d1190
Rodrigo Loche <rodrigoloche@gmail.com>
2025-12-23 14:55:08 +0900

    001

---
+++ desafio_01.py
@@ -0,0 +1,7 @@
+# Desafio 01
+
+# Crie um script Python que leia o nome de uma pessoa 
+# e mostre uma mensagem de boas-vindas de acordo com o valor digitado.
+
+nome = input("Qual é o seu nome? ")
+print(f"Bem vindo {nome}!") 
