4b37fec8a318acbddb2ba599b1691493cc678e2f
Rodrigo Loche <rodrigoloche@gmail.com>
2025-12-23 14:55:57 +0900

    003

---
+++ desafio_03.py
@@ -0,0 +1,10 @@
+# Desafio 03
+
+# Crie um script Python que leia dois números e mostre a soma entre eles.
+
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+sum = num1 + num2
+
+print(f"A soma de {num1} + {num2} = {sum}")
