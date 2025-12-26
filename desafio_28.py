46dc3219013fe3748f84221d93e5f174e316916f
Rodrigo Loche <rodrigoloche@gmail.com>
2025-12-26 20:16:05 +0900

    028

---
+++ desafio_28.py
@@ -0,0 +1,15 @@
+# Escreva um programa que faça o computador "Pensar" em uum número inteiro
+#entre 0 e 5 e peça para o usuário tentar descobrir qual foi o número escolhido pelo computador.
+
+# O programa deverá escrever na tela se o usuário venceu ou perdeu.
+
+import random
+
+num = random.randint(0, 5)
+guess = int(input("Adivinhe o número que o computador pensou de (0 á 5): "))
+
+if guess == num:
+    print("Você acertou!")
+else:
+    print(f"Você errou! o número era {num}")
+    
