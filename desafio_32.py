1bef9be03e79be4875cae7ac0667609f8ac7a7b6
Rodrigo Loche <rodrigoloche@gmail.com>
2025-12-27 18:13:22 +0900

    032

---
+++ desafio_32.py
@@ -0,0 +1,8 @@
+# faça um programa que leia um ano qualquer e mostre se ele é BISSEXTO.
+
+ano = int(input("Digite um ano: "))
+
+if (ano % 4 == 0 and ano % 100 != 0) or (ano % 400 == 0):
+    print(f"O ano de {ano} é Bissexto")
+else:
+    print(f"O ano de {ano} NÃO é bissexto")
